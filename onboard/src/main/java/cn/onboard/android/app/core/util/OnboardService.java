0124fa466d7691c8799d8a6c9c8567f6596f6edd
xuchen <xuchen0602@126.com>
2014-05-07 17:28:36 +0800

    MOD：add cache

--- onboard/src/main/java/cn/onboard/android/app/core/util/OnboardService.java
+++ onboard/src/main/java/cn/onboard/android/app/core/util/OnboardService.java
@@ -2,11 +2,13 @@ package cn.onboard.android.app.core.util;
 
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.http.HttpHeaders;
+import org.codehaus.jackson.type.TypeReference;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import cn.onboard.android.app.AppContext;
+import cn.onboard.android.app.cache.CacheDao;
+import cn.onboard.android.app.common.DataHandleUtil;
 
 /**
  * Created by XingLiang on 14-4-21.
@@ -17,8 +19,11 @@ public abstract class OnboardService {
 
     protected RestTemplate restTemplate;
 
+    protected CacheDao cacheDao;
+
     public OnboardService() {
         restTemplate = getRestTemplateUsingJacksonConverter();
+        cacheDao = AppContext.getDaoSession().getCacheDao();
     }
 
     protected String getUrl(String uri) {
@@ -35,5 +40,15 @@ public abstract class OnboardService {
         restTemplate.getMessageConverters().add(converter);
 
         return restTemplate;
+    }
+
+    public void saveData(String url,Object data){
+        String json = DataHandleUtil.objectToJson(data);
+
+    }
+
+    public <T> T readData(final TypeReference<T> type,String url){
+
+        return DataHandleUtil.StringToObject(type,url);
     }
 }
